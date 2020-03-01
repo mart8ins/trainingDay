61526119e237a39fd143f862db21b9274fe71239
Martins Skinkis <mart8ins@gmail.com>
2020-03-01 11:51:05 +0200

    Added JS functionality for Mondays popup window button and button for closing

--- scripts/scriptMonday.js
+++ scripts/scriptMonday.js
@@ -166,4 +166,21 @@ treadMillNav.addEventListener("click", function () {
 }, true);
 
 
+
 /*********************************************** POPUP WINDOW FOR TRAINING PROGRSS **************************************************/
+let buttonPop, popUpWindow, buttonPopClose;
+
+/* storing html elements */
+buttonPop = document.getElementById("train-progress-button-m");
+popUpWindow = document.getElementById("monday-pop");
+buttonPopClose = document.getElementById("monday-close-event");
+
+
+/* event listeneres */
+buttonPop.addEventListener('click', function () {
+    popUpWindow.style.display = "block";
+});
+buttonPopClose.addEventListener('click', function () {
+    popUpWindow.style.display = "none";
+})
+
