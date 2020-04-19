a59d5dc4d8624b3533d859aaaf869f71f2a4eb9d
Martins Skinkis <mart8ins@gmail.com>
2020-04-19 17:34:40 +0300

    Finished functionality of  storing data to localStorage and data controler

--- scripts/friday_training/scriptFridayPop.js
+++ scripts/friday_training/scriptFridayPop.js
@@ -25,26 +25,147 @@ let buttonController = (function () {
 })();
 
 
-
-
-let training = {
-    trainingDay: 'friday',
-    trainingData: {
-        trainingId: 1,
-        trainingDate: 2005 - 05 - 05,
-        trainingName: 'squats',
-        sets: {
-            set1: 1,
-            set2: 2,
-            set3: 3,
-            set4: 4
-        },
-        repetitions: {
-            rep1: 1,
-            rep2: 2,
-            rep3: 3,
-            rep4: 4
+{
+    let exercise = {
+        trainingDay: 'Friday',
+        trainingData: {
+            exerciseId: 1,
+            exerciseDate: '2005-05-05',
+            exerciseName: 'squats',
+            exerciseData: {
+                set: 'set 1',
+                weight: 22,
+                repetitions: 8
+            }
         }
     }
 }
+
+/**************************************
+ * FORM INPUT AND DATA OUTPUT
+ * ************************************/
+
+// REAL DATA CONTROLER
+let dataControler = (function () {
+
+    let training = [];
+
+    // object for exercise data storing
+    let Exercise = function (trainingDay, exerciseId, exerciseDate, exerciseName, set, weight, repetitions) {
+        this.trainingDay = trainingDay;
+        this.exerciseId = exerciseId;
+        this.exerciseDate = exerciseDate;
+        this.exerciseName = exerciseName;
+        this.set = set;
+        this.weight = weight;
+        this.repetitions = repetitions;
+    }
+
+    return {
+        addDataToTraining: function (trainingDay, exerciseId, exerciseDate, exerciseName, set, weight, repetitions) {
+            training.push(new Exercise(trainingDay, exerciseId, exerciseDate, exerciseName, set, weight, repetitions));
+        }
+    }
+})();
+
+
+/*********************************** */
+// CONTROLER FOR UI INPUT AND OUTPUT
+/************************************* */
+let UIcontroler = (function () {
+    return {
+        // get input data
+        getInputData: function () {
+            return {
+                exerciseDay: document.querySelector('.exercise__day').textContent,
+                exerciseId: UIcontroler.createID(),
+                exerciseDate: document.getElementById('exercise__date__input').value,
+                exerciseName: document.querySelector('.exercise__name').value,
+                exerciseSet: document.querySelector('.setValue').value,
+                exerciseWeight: document.getElementById('svars-0').value,
+                exerciseRepetitions: document.getElementById('rep-0').value,
+
+            }
+        },
+        // create ID for current Input data
+        createID: function () {
+            return Math.floor(Math.random() * 1000);
+        }
+    }
+})();
+
+
+
+/************************************ */
+// CONTROLER FOR DATA IN LOCAL STORAGE
+/*************************************** */
+let LScontroler = (function () {
+
+    return {
+        getFromLS: function () {
+            let trainingStorage;
+            if (localStorage.getItem('training') === null) {
+                trainingStorage = [];
+
+            } else {
+                trainingStorage = JSON.parse(localStorage.getItem('training'));
+            }
+            return trainingStorage;
+        },
+
+        storeToLS: function (val) {
+            let trainStorage = LScontroler.getFromLS();
+            trainStorage.push(val);
+            localStorage.setItem('training', JSON.stringify(trainStorage));
+
+        }
+    }
+})();
+
+
+
+/********************************* */
+// MAIN CONTROLLER
+/********************************** */
+let controler = (function (dataCTRL, uiCTRL, lsCTRL) {
+
+    function addItem(e) {
+        e.preventDefault();
+
+        // input data from UI controller
+        let input = uiCTRL.getInputData();
+
+        // created exercise ID
+        let inputID = uiCTRL.createID();
+
+        // storing input data with construcotr function in data array training
+        dataCTRL.addDataToTraining(input.exerciseDay, inputID, input.exerciseDate, input.exerciseName, input.exerciseSet, input.exerciseWeight, input.exerciseRepetitions);
+
+        // storing this data to localStorage
+        lsCTRL.storeToLS(input);
+    }
+
+    // all event listener function
+    let eventListeners = function () {
+        let sumbitbtn = document.getElementById('form__submit');
+        sumbitbtn.addEventListener('click', addItem);
+    }
+
+    return {
+        init: function () {
+            eventListeners();
+        }
+    }
+})(dataControler, UIcontroler, LScontroler);
+
+controler.init();
+
+
+
+
+
+
+
+
+
 
